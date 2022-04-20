60e5744fe4e1fed353b4494a021a80f3c8513fa8
GST-Slava <gstslava@gmail.com>
2022-04-20 21:50:27 +0300

    first commit

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Accordion(props: any) {
+    return <div>
+        <AccordionTitle title={props.titleValue}/>
+        <AccordionTitle title={props.titleValue}/>
+        <AccordionBody/>
+    </div>
+}
+
+function AccordionTitle(props: any) {
+    return (
+        <h3>{props.title}</h3>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+        </ul>
+    )
+}
+
+export default Accordion;
